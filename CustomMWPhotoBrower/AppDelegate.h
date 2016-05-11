b11800b878ddccdd8fdfdde149144f72d3e2f502
nhatdv_mobilus <nhatdv@mobilus.co.jp>
2016-05-11 15:01:26 +0900

    Initial Commit

---
+++ CustomMWPhotoBrower/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomMWPhotoBrower
+//
+//  Created by Dinh Van Nhat on 5/11/16.
+//  Copyright © 2016 Dinh Van Nhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomMWPhotoBrower/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomMWPhotoBrower
+//
+//  Created by Dinh Van Nhat on 5/11/16.
+//  Copyright © 2016 Dinh Van Nhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
